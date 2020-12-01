7970ac2cfca4cd11e879c07afd24b452db3f8589
Neilketchum <daipayanh@gmail.com>
2020-12-01 11:15:48 +0530

    Completed with Mail Service for sending Credentials and password

--- routes/addPoll.js
+++ routes/addPoll.js
@@ -2,45 +2,79 @@ const router = require("express").Router();
 const Polls = require("../models/Polls")
 const Voters = require("../models/Voters")
 const Candidates = require("../models/Candidates")
-const {auth} = require("./verifyToken");
+const { auth } = require("./verifyToken");
 const jwt = require("jsonwebtoken")
 const mongoose = require("mongoose")
-const {v4 : uuidv4} = require('uuid')
+const nodemailer = require("nodemailer")
+const { v4: uuidv4 } = require('uuid')
 
-router.post("/",auth, async (req,res)=>{
+router.post("/", auth, async (req, res) => {
     const token = req.header('auth-token');
-    const verified = jwt.verify(token,process.env.TOKEN_SECRET);
-    
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+
     const poll = new Polls({
-        admin:mongoose.Types.ObjectId(verified._id),
-        title:req.body.title,
-        positions:req.body.positions,
-        voters:req.body.voters
+        admin: mongoose.Types.ObjectId(verified._id),
+        title: req.body.title,
+        positions: req.body.positions,
+        voters: req.body.voters
     })
     // return res.send(req.body.positions)
     try {
         const savedPoll = await poll.save();
-        req.body.voters.map(async voter=>{
+        req.body.voters.map(async voter => {
             const vote = new Voters({
-                poll:poll._id,
-                voted:false,
-                name:voter.name,
-                email:voter.email,
-                password:uuidv4(),
-                year:voter.year
+                poll: poll._id,
+                voted: false,
+                name: voter.name,
+                email: voter.email,
+                password: uuidv4(),
+                year: voter.year
             })
+            let transporter = nodemailer.createTransport({
+                service:'gmail',
+                auth:{
+                    user:'neilketchumdev@gmail.com',
+                    pass:process.env.MAIL_PASS
+                }
+            });
+            let mailOptions = {
+                from:'neilketchumdev@gmail.com',
+                to:vote.email,
+                subject:'Test',
+                text:`Please Vote For what you belive in.Use this email and your password is ${vote.password}`
+            }
+            // send mail with defined transport object
+           await transporter.sendMail(mailOptions,(err,data)=>{
+                if(err){
+                    console.log("Errore",err)
+                }
+                else{
+                    console.log("Send")
+                }
+            })
+
+
+
+
+
+
+
+
+
+
+
             await vote.save();
         })
-        req.body.positions.map(async post=>{
-            post.candidates.map(async (candidate)=>{
+        req.body.positions.map(async post => {
+            post.candidates.map(async (candidate) => {
                 const person = new Candidates({
-                    poll:poll._id,
-                    position:post.name,
-                    votes:0,
-                    name:candidate.name,
-                    email:candidate.email,
-                    description:candidate.description,
-                    password:uuidv4(),
+                    poll: poll._id,
+                    position: post.name,
+                    votes: 0,
+                    name: candidate.name,
+                    email: candidate.email,
+                    description: candidate.description,
+                    password: uuidv4(),
                 })
                 await person.save()
             })
